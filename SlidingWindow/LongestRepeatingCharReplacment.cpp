e05e68e2903171b5e2f8ad446011f0df41e41f64
me21jarus <134506803+me21jarus@users.noreply.github.com>
2026-04-28 07:42:46 +0000

    SW

--- SlidingWindow/LongestRepeatingCharReplacment.cpp
+++ SlidingWindow/LongestRepeatingCharReplacment.cpp
@@ -1,71 +1,35 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        string ans = "";
-        int n = s.length();
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int ans = 0;
 
-        if(n < t.length() || s.empty() || t.empty()) return ans;
-        if(s == t) return s;
-
-        // unordered_map<char,int> mp;
-
-        // for(auto& c : t){
-        //     mp[c]++;
-        // }
-
-        // pair<int,int> res = {-1,-1};
-        // int resLen = INT_MAX;
-
+        //using 2 loops
         // for(int i=0;i<n;i++){
-        //     unordered_map<char,int> countS;
+        //     unordered_map<char,int> count;
+        //     int maxf = 0;
         //     for(int j=i;j<n;j++){
-        //         countS[s[j]]++;
-
-        //         bool flag = true;
-        //         for(auto &[c, cnt] : mp){
-        //             if(countS[c] < cnt){
-        //                 flag = false;
-        //                 break;
-        //             }
-        //         }
-        //         if(flag && (j-i+1) < resLen){
-        //             resLen = j-i+1;
-        //             res = {i,j};
+        //         count[s[j]]++;
+        //         maxf = max(maxf, count[s[j]]);
+        //         if((j-i+1) - maxf <= k){
+        //             ans = max(ans, j-i+1);
         //         }
         //     }
         // }
 
-        unordered_map<char,int> window, countT;
-        for(char c : t){
-            countT[c]++;
-        }
 
-        int have = 0, need = countT.size();
-        int resLen = INT_MAX;
-        int l = 0;
-        pair<int,int> res = {-1,-1};
-
-        for(int r = 0;r<n;r++){
-            char c = s[r];
-            window[c]++;
-
-            if(countT.count(c) && window[c] == countT[c]){
-                have++;
-            }
-
-            while(have == need){
-                if((r - l + 1) < resLen){
-                    resLen = r - l + 1;
-                    res = {l, r};
-                }
-                window[s[l]]--;
-                if(countT.count(s[l]) && window[s[l]] < countT[s[l]]){
-                    have--;
-                }
+        //using 1 loop sliding window
+        int l = 0, maxf = 0;
+        unordered_map<char,int> count;
+        for(int r = 0; r < n; r++){
+            count[s[r]]++;
+            maxf = max(maxf, count[s[r]]);
+            while((r-l+1) - maxf > k){
+                count[s[l]]--;
                 l++;
             }
+            ans = max(ans, r-l+1);
         }
-
-        return resLen == INT_MAX ? "" : s.substr(res.first,resLen);
+        return ans;
     }
 };
